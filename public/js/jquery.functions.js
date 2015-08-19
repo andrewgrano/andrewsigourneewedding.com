51d5e495da991b735b6ea01a41b21d3e72c8c042
andrewgrano <andrewgrano@gmail.com>
2015-08-18 23:11:11 -0700

    add parallax to two sections

--- public/js/jquery.functions.js
+++ public/js/jquery.functions.js
@@ -1,1 +1,23 @@
 var s = skrollr.init();
+
+
+// $(function() {
+//     var windowHeight = $(window).height();
+//     var windowHeightx1point5 = (windowHeight * 1.5);
+
+
+//     var url = $('.parallax ').css('background-image').replace('url(', '').replace(')', '').replace("'", '').replace('"', '');
+//     var bgImg = $('<img id="bgimgplaceholder" class="img-responsive">');
+//     bgImg.hide();
+//     bgImg.bind('load', function()
+//     {
+//         var height = $(this).height();
+//         // alert(height);
+//         $('.parallax').css('height',height);
+//         $('.parallax__buttonWrapper').css('height',height);
+//     });
+//     $('.parallax ').append(bgImg);
+//     bgImg.attr('src', url);
+
+
+// });
